42505ef13f72b88ee99143702b7d65f3430c0d63
PiyushBedre45 <113894461+PiyushBedre45@users.noreply.github.com>
2024-07-11 14:53:29 +0530

    post req

--- my-app/src/app/api/route.js
+++ my-app/src/app/api/route.js
@@ -1,7 +1,17 @@
 import { NextResponse } from "next/server";
 import { user } from '../../util/db'
+
 export function GET(req, res) {
     const data = user;
     console.log(data)
     return NextResponse.json(data)
+}
+
+export async function POST(req) {
+    let payload = await req.json()
+    console.log(payload.result)
+    if (!payload.result) {
+        return NextResponse.json({ result: "enter proper entries" })
+    }
+    return NextResponse.json({ result: "hi piyush" })
 }
--- my-app/src/app/mentors/[mentorsid]/page.js
+++ my-app/src/app/mentors/[mentorsid]/page.js
@@ -6,9 +6,7 @@ const MentorDetails = ({ params }) => {
     const [user, setUser] = useState(null);
     const getDetails = async () => {
         const response = await axios.get(`http://localhost:3000/api/${params.mentorsid}`)
-        console.log(response.data[0].name)
         setUser(response.data[0])
-        console.log('hi user name', user)
 
     }
     useEffect(() => {
